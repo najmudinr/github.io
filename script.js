937f5536f27aeac4b7394809498f9f53e5e79045
agillp <54714183+agillp@users.noreply.github.com>
2019-11-28 05:14:20 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -14,11 +14,17 @@ $(document).ready(function () {
         $(".form-login").css("display", "none");
         $(".form-signUp").css("display", "block");
     });
-    $(".close").click(function () {
-        $(".form-login").css("display", "none");
+    $("#form-linkLogin").click(function () {
+        $(".form-login").css("display", "block");
         $(".form-signUp").css("display", "none");
     });
+    $(".close").click(function () {
+        $(".form-login").css("display", "none");
+        $(".form-signUp").css("display", "block");
+    });
 });
+
+//Close form
 
 $(document).mouseup(function (e) {
     if ($(".form-login").is(e.target) || $(".form-signUp").is(e.target)) {
@@ -32,7 +38,7 @@ $(document).mouseup(function (e) {
 function validasi() {
     var username = document.forms["login"]["username"].value;
     var password = document.forms["login"]["password"].value;
-    if ((username == "agill" || username == "agill@gmail.com") && (password == "agill1234")) {
+    if ((username == "agill" || username == "agill@gmail.com" ) && (password == "agill1234")) {
         return true;
     } else {
         alert("Username atau password yang anda masukkan salah")
